e8d3e6d3dbc9073d069e87e05a0eeae32aea249e
Camilo Velez <camilov61@gmail.com>
2024-04-25 09:54:49 -0400

    fix: fix convert script wipp to clt

--- src/polus/tools/conversions/wipp_clt/convert.py
+++ src/polus/tools/conversions/wipp_clt/convert.py
@@ -1,4 +1,4 @@
-"""Convert WIPP Plugin to ICT."""
+"""Convert WIPP Plugin to CWL CommandLineTool."""
 
 from pathlib import Path
 from typing import Union
@@ -11,7 +11,7 @@ def wipp_to_clt(wipp: Union[Plugin, Path, str], out_path: Path, network_access: 
 
     plugin = wipp if isinstance(wipp, Plugin) else _load_plugin(wipp)
 
-    return (plugin._to_cwl(wipp, network_access=network_access),
+    return (plugin._to_cwl(network_access=network_access),
             plugin.save_cwl(out_path, network_access=network_access)
             )
 
